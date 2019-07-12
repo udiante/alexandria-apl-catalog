f626fd14e3e826bf84afc539a54d8febdf6aca4a
Alejandro Quibus <udiante@gmail.com>
2019-07-12 22:41:10 +0200

    Update APL Simple Icon Text

--- src/CATALOG/APL_Simple_Icon_Text/dataSource.js
+++ src/CATALOG/APL_Simple_Icon_Text/dataSource.js
@@ -1,7 +1,40 @@
-module.exports = {
+module.exports.LEGACY = APL_DATA_SOURCE
+
+const PublicDataSource = {
+    title: 'TITLE_TEXT', //Title of the APL,
+    mainText: 'STRING',
+    hintString: 'STRING',
+    backgroundImage: 'URL_BACKGROUND_IMAGE',
+    logoUrl: 'URL_LOGO_IMAGE',
+    style: {
+        backGroundOpacity: '0.7'
+    }
+}
+
+module.exports.DataSource = PublicDataSource
+
+/**
+ * Returns the APL datasource given the public data source
+ * @param {PublicDataSource} dataSource
+ * @returns {APL_DATA_SOURCE} APL Datasource response to the Alexa intent handler
+ */
+module.exports.getInternalDataSource = function(dataSource) {
+    var aplDataSource = Object.assign({}, APL_DATA_SOURCE)
+
+    aplDataSource.launchData.properties.headerTitle = dataSource.title
+    aplDataSource.launchData.properties.mainText = dataSource.mainText
+    aplDataSource.launchData.properties.hintString = dataSource.hintString
+    aplDataSource.launchData.properties.logoImage = dataSource.logoUrl
+    aplDataSource.launchData.properties.backgroundImage = dataSource.backgroundImage
+    aplDataSource.launchData.properties.backgroundOpacity = dataSource.style.backGroundOpacity
+
+    return aplDataSource
+}
+
+const APL_DATA_SOURCE = {
     launchData: {
         type: 'object',
-        properties: { // Custom properties of the view
+        properties: {
             headerTitle: "TITLE",
             mainText: "MAIN_TEXT",
             hintString: "HINT_STRING",
